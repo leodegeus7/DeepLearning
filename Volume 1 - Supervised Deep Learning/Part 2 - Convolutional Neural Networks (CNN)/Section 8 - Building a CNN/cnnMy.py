4b5e50fb8d7edf8601bc83f318c668f09f31d3df
Leonardo Geus <leoardodegeus@gmail.com>
2018-01-04 14:43:00 -0200

    Unit 8 - CNN Chapter

---
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnnMy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 24 19:02:01 2017
+
+@author: leodegeus
+"""
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
---
+++ Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/mySolution.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 24 11:42:37 2017
+
+@author: leodegeus
+"""
+
+from keras.models import Sequential
